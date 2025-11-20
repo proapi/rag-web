c6c82a42846ae1e665b3d97aed591c803a8889c9
Michał Pawelski <kontakt@proapi.eu>
2025-11-20 22:09:37 +0100

    feat: add async queries count on dashboard

--- app/models/query.rb
+++ app/models/query.rb
@@ -8,11 +8,22 @@ class Query < ApplicationRecord
   scope :successful, -> { where(error: nil) }
   scope :failed, -> { where.not(error: nil) }
 
+  # Broadcast stat updates after creating a query
+  after_create_commit :broadcast_stat_update
+
   def successful?
     error.nil?
   end
 
   def failed?
     !successful?
+  end
+
+  private
+
+  def broadcast_stat_update
+    broadcast_replace_to "queries_stats",
+      target: "queries_stat",
+      partial: "dashboard/queries_stat"
   end
 end
